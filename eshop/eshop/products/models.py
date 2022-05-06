d1a312843c76e6a8ffe21ce28faad7ee90d94a26
Evgeniy <devil_98@abv.bg>
2022-05-06 23:27:55 +0300

    created products and category models

--- eshop/eshop/products/models.py
+++ eshop/eshop/products/models.py
@@ -4,12 +4,36 @@ from django.db import models
 from eshop.core.validators import validate_only_numbers
 
 
+class Category(models.Model):
+    ORAL_CARE = 'oral care'
+    SKIN_CARE = 'skin care'
+    SUN_CARE = 'sun care'
+    HAIR_CARE = 'hair care'
+    DECORATIVE_COSMETICS = 'decorative cosmetics'
+    BODY_CARE = 'body care'
+    PERFUMES = 'perfumes'
+
+    ASSISTANT = [(x, x) for x in (ORAL_CARE, SKIN_CARE, SUN_CARE,
+                                  HAIR_CARE, DECORATIVE_COSMETICS,
+                                  BODY_CARE, PERFUMES)]
+
+    product_type = models.CharField(
+        max_length=max(len(x) for x, _ in ASSISTANT),
+        choices=ASSISTANT,
+    )
+
+    class Meta:
+        ordering = ('product_type', )
+
+
 class Product(models.Model):
     PRODUCT_NAME_MAX_LEN = 100
     PRODUCT_NAME_MIN_LEN = 3
 
-    oral care,\
-         skin care, sun care, hair care, decorative cosmetics, body care and perfumes.
+    LABEL_MAX_LEN = 50
+    LABEL_MIN_LEN = 3
+
+    TEXT_MAX_LENGTH = 300
 
     product_name = models.CharField(
         max_length=PRODUCT_NAME_MAX_LEN,
@@ -18,6 +42,15 @@ class Product(models.Model):
         ),
     )
 
+    product_label = models.CharField(
+        max_length=LABEL_MAX_LEN,
+        validators=(
+            MinLengthValidator(LABEL_MIN_LEN),
+        ),
+    )
+
+    product_image = models.ImageField()
+
     product_quantity = models.IntegerField(
         default=1,
         validators=(
@@ -25,4 +58,29 @@ class Product(models.Model):
         )
     )
 
-    product_type = models.CharField()
+    product_price = models.FloatField(
+        validators=(
+            validate_only_numbers,
+        ),
+    )
+
+    product_description = models.TextField(
+        max_length=TEXT_MAX_LENGTH,
+    )
+
+    available = models.BooleanField(
+        default=True,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
